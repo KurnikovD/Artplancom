ed73eb0e613281db86c28bdbbc8add6cdd0957f6
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-07 13:19:11 +0300

    add method for getting list of all animals and list of user animals

--- src/main/java/com/example/Artplancom/service/AnimalService.java
+++ src/main/java/com/example/Artplancom/service/AnimalService.java
@@ -5,7 +5,9 @@ import com.example.Artplancom.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class AnimalService {
@@ -16,5 +18,13 @@ public class AnimalService {
     public Animal findAnimalById(Long id){
         Optional<Animal> animal = animalRepository.findById(id);
         return animal.orElse(null);
+    }
+
+    public List<Animal> findAllAnimals(){
+        return animalRepository.findAll();
+    }
+
+    public  List<Animal> findAnimalsByUserId(Long userId){
+        return animalRepository.findByUser_Id(userId);
     }
 }
