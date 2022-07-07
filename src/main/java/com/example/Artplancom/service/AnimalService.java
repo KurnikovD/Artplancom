6dddf45c2a04fc945b32703f35451d90bb832340
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-07 12:49:40 +0300

    update AnimalService by adding findAnimalById method

--- src/main/java/com/example/Artplancom/service/AnimalService.java
+++ src/main/java/com/example/Artplancom/service/AnimalService.java
@@ -1,7 +1,20 @@
 package com.example.Artplancom.service;
 
+import com.example.Artplancom.entity.Animal;
+import com.example.Artplancom.repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AnimalService {
+
+    @Autowired
+    AnimalRepository animalRepository;
+
+    public Animal findAnimalById(Long id){
+        Optional<Animal> animal = animalRepository.findById(id);
+        return animal.orElse(null);
+    }
 }
