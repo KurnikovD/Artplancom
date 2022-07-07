bda2fdedee4d043d4c14c314365779e313dedc99
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-07 10:09:59 +0300

    Initial project

---
+++ src/main/java/com/example/Artplancom/ArtplancomApplication.java
@@ -0,0 +1,13 @@
+package com.example.Artplancom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtplancomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtplancomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Artplancom/ArtplancomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Artplancom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtplancomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
