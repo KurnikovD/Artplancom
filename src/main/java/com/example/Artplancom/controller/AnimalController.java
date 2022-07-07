d081a3da753a72e3337e162b0260a955c544f1ef
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-07 13:16:49 +0300

    Add login in AnimalController

--- src/main/java/com/example/Artplancom/controller/AnimalController.java
+++ src/main/java/com/example/Artplancom/controller/AnimalController.java
@@ -1,7 +1,50 @@
 package com.example.Artplancom.controller;
 
+import com.example.Artplancom.entity.Animal;
+import com.example.Artplancom.entity.User;
+import com.example.Artplancom.service.AnimalService;
+import com.example.Artplancom.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AnimalController {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    AnimalService animalService;
+
+    @GetMapping("/animal")
+    public String animalList(Model model){
+        List<Animal> allAnimals = animalService.findAllAnimals();
+
+        if (allAnimals.isEmpty()){
+            model.addAttribute("EmptyList", "Список пуст");
+        }else {
+            model.addAttribute("animalsList", allAnimals);
+        }
+
+        return "animals";
+
+    }
+
+    @GetMapping("/my_animal")
+    public String animalUserList(Model model, User user){
+
+        List<Animal> allAnimals = animalService.findAnimalsByUserId(user.getId());
+
+        if (allAnimals.isEmpty()){
+            model.addAttribute("EmptyList", "Ваш список пуст");
+        }else {
+            model.addAttribute("animalsList", allAnimals);
+        }
+
+        return "animals";
+
+    }
 }
