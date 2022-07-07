848c8301124c337eee13ba60a91554491a0246af
KurnikovD <34297950+KurnikovD@users.noreply.github.com>
2022-07-07 13:17:26 +0300

    add query for finding animals by user id

--- src/main/java/com/example/Artplancom/repository/AnimalRepository.java
+++ src/main/java/com/example/Artplancom/repository/AnimalRepository.java
@@ -5,14 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
     @Override
     Optional<Animal> findById(Long id);
-
     long deleteByNickName(String nickName);
-
+    List<Animal> findByUser_Id(Long id);
 
 }
